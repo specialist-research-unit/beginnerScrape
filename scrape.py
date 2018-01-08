44c243f6947f1bd89392abcd01c4c489f95eea87
jack <jack.burrus@thomsonreuters.com>
2018-01-08 15:28:05 -0500

    Code written

--- scrape.py
+++ scrape.py
@@ -1,3 +1,17 @@
 #This is a beginner web scrape that you can try on websites you frequently visit.
 
-#Please install the following plugins for this code to run.
+
+from bs4 import BeautifulSoup as bs
+import urllib
+import prettytable as pt
+
+
+#AlJazeera
+web_page = urllib.urlopen('http://www.aljazeera.com/topics/country/iraq.html').read()
+soup = bs(web_page, "lxml")
+print("________________Al Jazeera_______________")
+def loop():
+    for title in soup.find_all('h2'):
+        print(("-")+title.get_text().encode("utf-8"))
+AlJazeera_Data = loop()
+print(AlJazeera_Data)
